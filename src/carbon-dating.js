5c0092f5d9ecc0a0ab603153580da82d449e4b96
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-03-30 02:58:18 +0600

    fix: fixed error carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,15 +1,19 @@
+const CustomError = require("../extensions/custom-error");
+
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  const sampleActiveNumber = Number(sampleActivity); 
-    if (isNaN(sampleActiveNumber)                 || 
-        sampleActiveNumber <= 0                   || 
-        sampleActiveNumber > 15                   || 
-        typeof sampleActivity !== String) {
+  let result;
+  if (typeof sampleActivity == "object" || typeof sampleActivity == "number") {
       return false;
-    } else {
-      const yearCnt = (Math.log((MODERN_ACTIVITY / sampleActiveNumber) / (0.639 / HALF_LIFE_PERIOD)));
-      return yearCnt;
-    }
+  }
+
+  if (sampleActivity && parseFloat(sampleActivity) && (sampleActivity > 0 && sampleActivity < MODERN_ACTIVITY)) {
+      result = Math.ceil((Math.log((MODERN_ACTIVITY / sampleActivity)) / 0.693) * HALF_LIFE_PERIOD);
+      return result;
+  } else {
+      return false;
+  }
+  
 };

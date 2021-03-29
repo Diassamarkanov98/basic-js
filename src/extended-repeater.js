de6c2785d152c1f423ef25a85d54c3151fc1935b
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-03-29 17:38:56 +0600

    feat: finish extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	if (!options.hasOwnProperty('separator')) {
+		options.separator = '+';
+	 }
+	 if (!options.hasOwnProperty('additionSeparator') && options.hasOwnProperty('additionRepeatTimes')) {
+		options.additionSeparator = '|';
+	 }
+	 if (options.hasOwnProperty('addition') && options.addition === null) {
+			options.addition = 'null';
+	 }
+	 let addArr = new Array(options.additionRepeatTimes).fill(options.addition);
+	 let addStr = addArr.join(options.additionSeparator);
+	 let partStr = str + addStr;
+	 let resultArr = new Array(options.repeatTimes).fill(partStr);
+	 let result = resultArr.join(options.separator);
+  return result;
 };
-  
